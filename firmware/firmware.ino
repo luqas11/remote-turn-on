420935b7de4345059d46e02bfc3b32b55ec1a0c9
luqas11 <lucascazas@hotmail.com>
2025-02-28 21:10:00 -0300

    Add firmware sketch

---
+++ firmware/firmware.ino
@@ -0,0 +1,43 @@
+#include <ESP8266WiFi.h>
+#include "config.h"
+#include <BlynkSimpleEsp8266.h>
+#include <WiFiUdp.h>
+#include <WakeOnLan.h>
+
+// Pin of the LED to be controlled
+int LED_PIN = 2;
+
+WiFiUDP UDP;
+WakeOnLan WOL(UDP);
+
+// Listener to the Blynk Virtual Pin V0
+BLYNK_WRITE(V0)
+{
+  // Get the Virtual Pin current value
+  int value = param.asInt();
+
+  // Set the LED to the status of the Virtual Pin
+  digitalWrite(LED_PIN, !value);
+
+  // If the Virtual Pin is HIGH, send the Wake On LAN magic packet to a given MAC address
+  if (value){
+    const char *MACAddress = "6C:2B:59:40:22:AA";
+    WOL.sendMagicPacket(MACAddress);
+  }
+}
+
+void setup()
+{
+  // Initialize the LED turned off
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  // Connect the board to the Blynk Cloud
+  Blynk.begin(BLYNK_AUTH_TOKEN, SSID, PASSWORD);
+}
+
+void loop()
+{
+  // Run the Blynk listeners
+  Blynk.run();
+}
